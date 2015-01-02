43c8923c8285d7a5cb4c246d5968250261b6efad
MTRNord <mtrnord1@gmail.com>
2015-01-02 21:13:27 +0100

    try to fix buttons for the loop

--- post_footer.php
+++ post_footer.php
@@ -1,1 +1,23 @@
+  <script type="text/javascript" src="../../core/backend/admin/modules/share_buttons/socialshareprivacy/js/jquery.socialshareprivacy.js"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function($){
+      if($('#socialshareprivacy').length > 0){
+        $('#socialshareprivacy').socialSharePrivacy({
+          services : {
+            facebook : {
+              'perma_option' : 'on'
+            }, 
+            twitter : {
+              'status' : 'on'
+            },
+            gplus : {
+            }
+          },
+          "css_path"  : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/socialshareprivacy.css",
+          "lang_path" : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/lang/",
+          "language"  : "de"
+        });
+      }
+    });
+  </script>
 <div id="socialshareprivacy"></div>
