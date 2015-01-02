3059ee56680462c8d687d059531b276f5fd7f6a7
MTRNord <mtrnord1@gmail.com>
2015-01-02 22:18:24 +0100

    new try

--- html_header.php
+++ html_header.php
@@ -6,21 +6,23 @@
       var i = 1
       //while (i < divs){
         if($('#socialshareprivacy').length > 0){
-          $("#socialshareprivacy").each($('#socialshareprivacy').socialSharePrivacy({
-            services : {
-              facebook : {
-                'perma_option' : 'on'
-              }, 
-              twitter : {
-                'status' : 'on'
+          $("#socialshareprivacy").each(function(){
+            $('#socialshareprivacy').socialSharePrivacy({
+              services : {
+                facebook : {
+                  'perma_option' : 'on'
+                }, 
+                twitter : {
+                  'status' : 'on'
+                },
+                gplus : {
+                }
               },
-              gplus : {
-              }
-            },
-            "css_path"  : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/socialshareprivacy.css",
-            "lang_path" : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/lang/",
-            "language"  : "de"
-          }));
+              "css_path"  : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/socialshareprivacy.css",
+              "lang_path" : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/lang/",
+              "language"  : "de"
+            });
+          });
         }
         i++;
       //}
