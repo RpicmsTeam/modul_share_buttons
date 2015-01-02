5c1c959ac872ab38b66e5ee245f15702c75db37c
MTRNord <mtrnord1@gmail.com>
2015-01-02 21:17:19 +0100

    another try

--- html_header.php
+++ html_header.php
@@ -1,6 +1,6 @@
   <script type="text/javascript" src="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js"></script>
   <script type="text/javascript" src="../../core/backend/admin/modules/share_buttons/socialshareprivacy/js/jquery.socialshareprivacy.js"></script>
-  <script type="text/javascript">
+  <!--<script type="text/javascript">
     jQuery(document).ready(function($){
       //if($('#socialshareprivacy').length > 0){
         $('#socialshareprivacy').socialSharePrivacy({
@@ -20,4 +20,4 @@
         });
       //}
     });
-  </script>
+  </script>-->
--- post_footer.php
+++ post_footer.php
@@ -1,1 +1,22 @@
+  <script type="text/javascript">
+    jQuery(document).ready(function($){
+      //if($('#socialshareprivacy').length > 0){
+        $('#socialshareprivacy').socialSharePrivacy({
+          services : {
+            facebook : {
+              'perma_option' : 'on'
+            }, 
+            twitter : {
+              'status' : 'on'
+            },
+            gplus : {
+            }
+          },
+          "css_path"  : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/socialshareprivacy.css",
+          "lang_path" : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/lang/",
+          "language"  : "de"
+        });
+      //}
+    });
+  </script>
 <div id="socialshareprivacy"></div>
