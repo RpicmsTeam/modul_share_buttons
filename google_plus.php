bfba827d34b3e4211617fe58ad0e1a4177b7cb7a
MTRNord <mtrnord1@gmail.com>
2015-01-02 20:33:17 +0100

    initial commit

---
+++ google_plus.php
@@ -0,0 +1,3 @@
+<link href="path-to-file/social-sharing.css" rel="stylesheet">
+
+<a href="https://plus.google.com/share?url=" title="Share on Google+" target="_blank" class="btn btn-googleplus"><i class="fa fa-google-plus"></i> Google+</a>
