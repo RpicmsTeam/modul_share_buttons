1a834b1ce3315039eded3e66071b63c47b9816fe
MTRNord <mtrnord1@gmail.com>
2015-01-03 11:33:44 +0100

    cleanup code

--- google_plus.php
+++
@@ -1,3 +0,0 @@
-<link href="path-to-file/social-sharing.css" rel="stylesheet">
-
-<a href="https://plus.google.com/share?url=" title="Share on Google+" target="_blank" class="btn btn-googleplus"><i class="fa fa-google-plus"></i> Google+</a>
